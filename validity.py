4cc3e3e0d79a6b6595b52a87f3bcc8d619e23fb1
Misael Pelayo <134965888+AngelMisaelPelayo@users.noreply.github.com>
2024-02-09 22:33:26 -0600

    Update validity.py

--- validity.py
+++ validity.py
@@ -5,29 +5,31 @@ from Tree2SMT-LIB import fol_tree_to_smtlib, extract_variables
 def check_fol_validity(premises, conclusion):
     # Initialize the solver
     solver = Solver()
-
+    DicPredicates={}
     # Convert all premises to SMT-LIB and add to solver
     for premise in premises:
+        #print(SetPredicates)
         variables=extract_variables(premise)
-        premise_tree = parse_text_FOL_to_tree(premise)
-        premise_smtlib = fol_tree_to_smtlib(premise_tree, variables)
+        premise_smtlib = fol_tree_to_smtlib(premise, variables,DicPredicates)
         #print(premise_smtlib)
         solver.add(parse_smt2_string(premise_smtlib))
-      # Parse the SMT-LIB string into a Z3 expression
+    solver.push()
+    # Parse the SMT-LIB string into a Z3 expression
     # Convert conclusion to SMT-LIB
-    conclusion_tree = parse_text_FOL_to_tree(conclusion)
     variables=extract_variables(conclusion)
-    conclusion_smtlib = fol_tree_to_smtlib(conclusion_tree,variables)
+    conclusion_smtlib = fol_tree_to_smtlib(conclusion,variables,DicPredicates)
     conclusionS = parse_smt2_string(conclusion_smtlib)
-    #print(conclusionS)
-    #print(solver)
     negated_conclusion=Not(conclusionS[0])
     # Add the negation of the conclusion to the solver
     solver.add(negated_conclusion)
+
     # Check if the negated conclusion is unsatisfiable with the premises
     if solver.check() == unsat:
         return 'True'
-    elif solver.check() == sat:
-        return 'False'
     else:
-        return 'Unknown'
+        solver.pop()
+        solver.add(conclusionS[0])
+        if solver.check() == unsat:
+            return 'False'
+        else:
+            return 'Unknown'
